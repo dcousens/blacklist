a212fbbfac130fb3f18610bf5ce517e16d10ae61
Jed Watson <jed.watson@me.com>
2015-03-27 15:36:57 +1100

    Adding test for string arguments

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@
 var assert = require('assert')
 var blacklist = require('../')
 
-describe('blacklist', function() {
-  it('works as expected', function() {
+describe('blacklist', function () {
+  it('blacklists properties in a filter object', function () {
     var someInput = { a: 1, b: 2, c: 3 }
     var result = blacklist(someInput, {
       a: true,   // a will not be in the result
@@ -13,5 +13,11 @@ describe('blacklist', function() {
     })
 
     assert.deepEqual(result, { b: 2, c: 3 })
+  })
+  it('accepts n string arguments', function () {
+    var someInput = { a: 1, b: 2, c: 3 }
+    var result = blacklist(someInput, 'b', 'c')
+
+    assert.deepEqual(result, { a: 1 })
   })
 })
