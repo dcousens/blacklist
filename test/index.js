3f67bb0b223579763e6fdbb7f024826616a5faad
Daniel Cousens <github@dcousens.com>
2016-07-13 14:19:39 +1000

    use tape/faucet instead

--- test/index.js
+++ test/index.js
@@ -1,24 +1,22 @@
-/* global describe it */
-
-var assert = require('assert')
 var blacklist = require('../')
+var tape = require('tape')
 
-describe('blacklist', function () {
-  it('omits properties from a filter object', function () {
-    var someInput = { a: 1, b: 2, c: 3 }
-    var result = blacklist(someInput, {
-      a: true,
-      b: false,
-      c: false
-    })
-
-    assert.deepEqual(result, { b: 2, c: 3 })
+tape('omits properties from a filter object', function (t) {
+  var someInput = { a: 1, b: 2, c: 3 }
+  var result = blacklist(someInput, {
+    a: true,
+    b: false,
+    c: false
   })
 
-  it('omits properties from an arguments list', function () {
-    var someInput = { a: 1, b: 2, c: 3 }
-    var result = blacklist(someInput, 'b', 'c')
+  t.deepEqual(result, { b: 2, c: 3 })
+  t.end()
+})
 
-    assert.deepEqual(result, { a: 1 })
-  })
+tape('omits properties from an arguments list', function (t) {
+  var someInput = { a: 1, b: 2, c: 3 }
+  var result = blacklist(someInput, 'b', 'c')
+
+  t.deepEqual(result, { a: 1 })
+  t.end()
 })
